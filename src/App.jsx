38b59a3ad7b68e16545543bc5abf1be9ba12a593
suvo <suvodatta72@gmail.com>
2024-10-21 23:26:51 +0600

    initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-[2rem] bg-green-50 p-4 m-4 text-center font-bold'>React</h1>
+    </>
+  )
+}
+
+export default App
